7e3d58f5d90f8ca2f86634b128a59d9abbbeb622
paslavsky <a.paslavsky@gmail.com>
2020-03-05 12:07:34 +0000

    Deploying to gh-pages from  - 05c527fa252688be98d1afea76260581a747207d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ef6d4b444e949be2dc9b4c6fb396a7b.js"
+  "/java-microservices/precache-manifest.144b70ae6db6f881a37f9ad40b9d0e4b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/java-microservices/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
