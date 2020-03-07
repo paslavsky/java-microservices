efc9fa00267f942618f735dda2cee25223fd7ab8
paslavsky <a.paslavsky@gmail.com>
2020-03-07 14:53:13 +0000

    Deploying to gh-pages from  - aca385380087980d2ebdc062b162b20335c14f8e 🚀

--- precache-manifest.60465145736921ef550f23bc83274000.js
+++ precache-manifest.60465145736921ef550f23bc83274000.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "159a719c1db6cd0a58c3ea5c104416be",
+    "revision": "88acc583e404507232336f16b559a2ae",
     "url": "/java-microservices/index.html"
   },
   {
-    "revision": "b16254dd875eac5dcf50",
+    "revision": "9a698ace3ffac29bc483",
     "url": "/java-microservices/static/css/2.9610bb6c.chunk.css"
   },
   {
-    "revision": "be60a588330d3a4446db",
-    "url": "/java-microservices/static/css/main.79668e06.chunk.css"
+    "revision": "79b58e1ab4938b4631e7",
+    "url": "/java-microservices/static/css/main.c49dba36.chunk.css"
   },
   {
-    "revision": "b16254dd875eac5dcf50",
-    "url": "/java-microservices/static/js/2.1c1ce598.chunk.js"
+    "revision": "9a698ace3ffac29bc483",
+    "url": "/java-microservices/static/js/2.2d0864fe.chunk.js"
   },
   {
     "revision": "14b43d3b8bf07a0152720664793b77a0",
-    "url": "/java-microservices/static/js/2.1c1ce598.chunk.js.LICENSE.txt"
+    "url": "/java-microservices/static/js/2.2d0864fe.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "be60a588330d3a4446db",
-    "url": "/java-microservices/static/js/main.7125d4af.chunk.js"
+    "revision": "79b58e1ab4938b4631e7",
+    "url": "/java-microservices/static/js/main.f4f1976c.chunk.js"
   },
   {
     "revision": "2953cc678252369e2f4a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/java-microservices/precache-manifest.db7b6bf43c9a4ff6853f87d0a42acdd0.js"
+  "/java-microservices/precache-manifest.60465145736921ef550f23bc83274000.js"
 );
 
 self.addEventListener('message', (event) => {
